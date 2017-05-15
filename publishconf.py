d57e2ec2888bd83ec8ebf741cd71db26f86281ae
Your Name <you@example.com>
2017-05-15 21:12:55 +0800

    add blog

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://www.njcxs.cc'
+SITEURL = 'http://www.njcxs.cc'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
